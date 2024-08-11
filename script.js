b0b3e9bbb103aa64b9ebf936b40ed8e09f2da9da
KISHOTH KUMAR <79006624+Kishoth-45@users.noreply.github.com>
2024-08-11 18:38:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -59,4 +59,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Initialize WOW.js for animations
     new WOW().init();
+
+
+
+    const header = document.querySelector('.header');
+    let lastScrollTop = 0;
+
+    const handleScroll = () => {
+      let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+      if (currentScroll > lastScrollTop) {
+        header.classList.add('hidden');
+      } else {
+        header.classList.remove('hidden');
+      }
+      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    };
+
+    const handleKeyUp = (event) => {
+      if (event.key === 'PageUp') {
+        header.classList.remove('hidden');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('keyup', handleKeyUp);
 });
